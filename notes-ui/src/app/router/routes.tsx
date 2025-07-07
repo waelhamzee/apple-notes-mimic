51b723a291a80b81326ac7bb2a529efb603027b9
waelhamzee <waelhamzee0@gmail.com>
2025-07-08 01:58:20 +0300

    add route handling

--- notes-ui/src/app/router/routes.tsx
+++ notes-ui/src/app/router/routes.tsx
@@ -1,13 +1,20 @@
+import { useAuth } from "@/features/auth/hooks/useAuth";
 import MainLayout from "@/features/auth/MainLayout";
 import AuthPage from "@/features/auth/pages/AuthPage";
 import NotFoundPage from "@/features/common/pages/NotFoundPage";
 import NotesPage from "@/features/notes/pages/NotesPage";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import ProtectedRoute from "./ProtectedRoute";
+
+const RootRedirect = () => {
+  const { isAuthenticated } = useAuth();
+  return <Navigate to={isAuthenticated ? "/notes" : "/auth"} replace />;
+};
 
 const AppRoutes = () => {
   return (
     <Routes>
+      <Route path="/" element={<RootRedirect />} />
       <Route
         path="/auth"
         element={
